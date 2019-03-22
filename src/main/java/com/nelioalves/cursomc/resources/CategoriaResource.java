29835ef60f9a0e9b01f4871bdf42c550fd7cca96
yankev <yankevmineo@gmail.com>
2019-03-21 22:10:42 -0300

    criando o model categoria

--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -1,14 +1,28 @@
 package com.nelioalves.cursomc.resources;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.nelioalves.cursomc.models.Categoria;
 
 @RestController
 @RequestMapping(value="/categoria")
 public class CategoriaResource {
 	
 	@RequestMapping()
-	public String listar() {
-		return "Está funcionando o mapping";
+	public List<Categoria> listar() {
+		
+		Categoria cat = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(1, "Escritório");
+		
+		List<Categoria> list = new ArrayList<Categoria>();
+		list.add(cat);
+		list.add(cat2);
+		
+		return list;
 	}
 }
